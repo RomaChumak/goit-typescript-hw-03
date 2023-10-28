7b3e05081fe9ad7f87f735d9f653532b8b7877ab
RomaChumak <romachumak355@gmail.com>
2023-10-28 18:08:05 +0300

    1

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+class Key {
+    private signature: number;
+
+    constructor(){
+        this.signature = Math.random();
+    }
+      getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+    private key: Key;
+
+    constructor(key: Key) {
+        this.key = key; 
+    }
+
+    getKey(): Key {
+    return this.key
+    }
+ }
+
+abstract class MHouse{
+    door: boolean;
+    key: Key;
+    tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.door = false;
+        this.key = key;
+    }
+    comeIn(person: Person) {
+       this.door
+      ? this.tenants.push(person)
+      : alert("Sorry, but the door is closed");
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends MHouse{
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    } else {
+      this.door = false;
+    }
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
